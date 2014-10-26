8c94b5902c5473f7c796c5658bfe7ad6fde5c762
ericak11 <kantor.erica@gmail.com>
2014-10-25 21:31:49 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+
+var app = require('express')();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+var SpotifyWebApi = require('spotify-web-api-node');
+var spotifyApi = new SpotifyWebApi();
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('parse spotify', function(item){
+    spotifyApi.searchTracks(item)
+      .then(function(data) {
+        var song = data.tracks.items[Math.floor(Math.random()*data.tracks.items.length)];
+        io.emit('parse spotify', song);
+      }, function(err) {
+        console.error(err);
+      });
+  });
+});
