c5881751d7b52896bdd251df143c7b0d16bf044d
ericak11 <kantor.erica@gmail.com>
2014-10-26 13:28:15 -0400

    added hints

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 
 var app = require('express')();
 var http = require('http');
+var https = require('https');
 var server = http.createServer(app);
 var io = require('socket.io')(server);
+
+
 var SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi();
+
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 server.listen(server_port, server_host);
